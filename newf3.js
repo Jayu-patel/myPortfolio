8988bfdb5ca7224cbbeb6adf57c099b05e305a9f
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-03-12 23:01:37 +0530

    Add files via upload

--- newf3.js
+++ newf3.js
@@ -2,8 +2,9 @@ const btn = document.querySelector("#btn")
 const closebtn = document.querySelector("#closebtn")
 const link = document.querySelectorAll(".alink")
 const list = document.querySelector(".nav_links")
-
+let n = 0
 btn.addEventListener('click', () => {
+    n = 1
     closebtn.style.display = "block"
     btn.style.display = "none"
     list.style.width = '200px'
@@ -13,6 +14,7 @@ btn.addEventListener('click', () => {
 })
 
 closebtn.addEventListener('click', () => {
+    n = 0
     btn.style.display = "block"
     closebtn.style.display = "none"
     list.style.width = '0px'
@@ -23,11 +25,17 @@ closebtn.addEventListener('click', () => {
 
 const closenav = [
     document.querySelector('.sec'),
-    document.querySelector('.skills')
+    document.querySelector('.skills'),
+    document.querySelector('.qualification'),
+    document.querySelector('.project')
 ]
 
 closenav.forEach(e => {
     e.addEventListener('click', () => {
+        if(n == 0){
+            return
+        }
+        console.log("ok")
         btn.style.display = "block"
         closebtn.style.display = "none"
         list.style.width = '0px'
